9aba195d4e070a1efb83150d61fe00ec22505931
teodorescurazvan <teodorescurazvan@yahoo.com>
2020-09-01 22:36:06 +0300

    Update fetchContent.js

--- src/layout/fetchContent.js
+++ src/layout/fetchContent.js
@@ -1,14 +1,14 @@
 /* Define front-end properties - Start */
 const token = 'a5e4f2d801eb4d338a7aad40a86544d5'
 const taxonomyCatName = 'Game Highlights'
-const containerAssetID = ''
+const matchHeroHeaderId = 'CORE0C26D9F3D73D4471BA72C0C8DE501087'
 const host = 'https://demo-oce0002.cec.ocp.oraclecloud.com/'
 /* Define front-end properties - End */
 
 const itemsURL = ({ maxResults, sortOrder }) =>
-  `${host}/content/published/api/v1.1/items?orderBy=${esc(sortOrder)}&limit=${maxResults}&channelToken=${token}&q=(type eq "Story" AND taxonomies.categories.name eq "${taxonomyCatName}")`
+  `${host}/content/published/api/v1.1/items?orderBy=${esc(sortOrder)}&limit=${maxResults}&channelToken=${token}&q=(type eq "Story" OR type eq "Match-Hero-Header" AND taxonomies.categories.name eq "${taxonomyCatName}")`
 
-const esc = encodeURIComponent
+  const esc = encodeURIComponent
 
 const noDigitalAssets = e => e.type !== 'DigitalAsset'
 
