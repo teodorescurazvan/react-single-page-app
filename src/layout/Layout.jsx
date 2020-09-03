ccf46002da52249accf9d0b7008201f4f298c1db
teodorescurazvan <teodorescurazvan@yahoo.com>
2020-09-03 22:30:46 +0300

    updated

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,12 +4,12 @@ import './raleway.css'
 
 import fetchContent from './fetchContent'
 
-const dump = d => {
+/* const dump = d => {
   console.log(d)
   return d
-}
+} */
 const items = () => fetchContent()
-
+console.log(items);
 const toHref = da => da.fields.native.links[0].href
 
 export default class Layout extends Component {
@@ -22,14 +22,16 @@ export default class Layout extends Component {
   }
 }
 const w100pct = { width: '100%' }
-const w50px = { width: '50px' }
+const w50pct = { width: '50%' }
 
 const Body = () => (
   <div className='w3-light-grey'>
     <div className='w3-content' style={{ maxWidth: '1400px' }}>
       <Header />
       <div className='w3-row'>
+        <div className='w3-col l12 m12 s12'>
           <Blogs />
+        </div>
       </div>
       <br />
     </div>
@@ -38,8 +40,8 @@ const Body = () => (
 )
 
 const Header = () => (
-  <header className='w3-container w3-padding-32'>
-    <Banner />
+  <header className='w3-container w3-center w3-padding-32'>
+      
   </header>
 )
 
@@ -47,62 +49,12 @@ const Footer = () => (
   <footer className='w3-container w3-dark-grey w3-padding-32 w3-margin-top'>
     <p>
       Powered by{' '}
-      <a href='http://www.oracle.com' target='_blank' rel="noopener noreferrer">
-        Oracle
+      <a href='https://www.w3schools.com/w3css/default.asp'>
+        w3.css
       </a>
     </p>
   </footer>
 )
-
-/* const AboutCard = () => (
-  <div className='w3-card w3-margin w3-margin-top'>
-    <img src='/w3images/supporter_club.jpg' style={w100pct} alt="supporter club" />
-    <div className='w3-container w3-white'>
-      <h4>
-        <b>Tell us the story of your supporters club</b>
-      </h4>
-      <p>
-      A brief history of how the Supporters' Club came to be, the biggest 
-      events you've attended together as group… Don't forget details like 
-      the founding year, number or members, and the board of directors.
-      </p>
-    </div>
-  </div>
-) */
-
-/* const PopularPosts = () => (
-  <div className='w3-card w3-margin'>
-    <div className='w3-container w3-padding'>
-      <h4>Popular Posts</h4>
-    </div>
-    <ul className='w3-ul w3-hoverable w3-white'>
-      <li className='w3-padding-16'>
-        <img src='/w3images/workshop.jpg' alt='workshop' className='w3-left w3-margin-right' style={w50px} />
-        <span className='w3-large'>Lorem</span>
-        <br />
-        <span>Sed mattis nunc</span>
-      </li>
-      <li className='w3-padding-16'>
-        <img src='/w3images/gondol.jpg' alt='gondol' className='w3-left w3-margin-right' style={w50px} />
-        <span className='w3-large'>Ipsum</span>
-        <br />
-        <span>Praes tinci sed</span>
-      </li>
-      <li className='w3-padding-16'>
-        <img src='/w3images/skies.jpg' alt='skies' className='w3-left w3-margin-right' style={w50px} />
-        <span className='w3-large'>Dorum</span>
-        <br />
-        <span>Ultricies congue</span>
-      </li>
-      <li className='w3-padding-16 w3-hide-medium w3-hide-small'>
-        <img src='/w3images/rock.jpg' alt='rock' className='w3-left w3-margin-right' style={w50px} />
-        <span className='w3-large'>Mingsum</span>
-        <br />
-        <span>Lorem ipsum dipsum</span>
-      </li>
-    </ul>
-  </div>
-) */
 
 class Blogs extends Component {
   constructor (props) {
@@ -113,57 +65,27 @@ class Blogs extends Component {
     items().then(data => {
         console.log(data)
         
-        const blogs = data.ALL.items.filter(
-          item => item.type === 'Story'
+        const banner = data.ALL.items.filter(
+          item => item.type === 'Match-Hero-Header'
         )
+        console.log(banner);
         const items = data.items
         this.setState({ items:items })
+        this.setState({ banner:banner }) 
+        console.log(this)
       })
       .catch(err => {
         console.error(err)
         this.setState({ err: err.toString() })
       })
   }
-  render () {
+  render () {   
     const child = this.state.err ? (
-      <ErrorComponent err={this.state.err} />
+    <ErrorComponent err={this.state.err} />
     ) : this.state.items.length === 0 ? (
-      <NoItems />
+    <NoItems />
     ) : (
-      <List items={this.state.items} />
-    )
-    return child
-  }
-}
-
-class Banner extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { items: [] }
-  }
-  componentDidMount() {
-    items().then(data => {
-      console.log(data)
-      
-      const banner = data.ALL.items.filter(
-        item => item.type === 'Match-Hero-Header'
-      )
-      const items = data.items
-      this.setState({ items:items })
-    })
-    .catch(err => {
-      console.error(err)
-      this.setState({ err: err.toString() })
-    })
-  }
-  render () {
-    const child = this.state.err ? (
-      <ErrorComponent err={this.state.err} />
-    ) : this.state.items.length === 0 ? (
-      <NoItems />
-    ) : (
-      <List items={this.state.items} />
-    )
+    <Banner item={this.state.banner} /> && <List items={this.state.items} />)
     return child
   }
 }
@@ -181,6 +103,7 @@ const List = props => {
     </div>
   )
 }
+
 const dateToMDY = date =>
   new Date(date.value).toLocaleDateString('en-US', {
     year: 'numeric',
@@ -192,7 +115,6 @@ const Blog = ({ item }) => {
   const { fields } = item
   const {
     author,
-    body,
     media,
     publishDate,
     subtitle
@@ -217,3 +139,29 @@ const Blog = ({ item }) => {
     </div>
   )
 }
+
+const Banner = ({ item }) => {
+  const { fields } = item
+  const {
+    team_1_name,
+    team_1_logo,
+    team_2_name,
+    team_2_logo,
+    banner,
+    score
+  } = fields
+
+  const team_1_logo_url = toHref(team_1_logo)
+  const team_2_logo_url = toHref(team_2_logo)
+  const banner_url = toHref(banner)
+
+return (
+  <div>
+    <img src={team_1_logo_url} alt={item.name} style={w50pct} />
+    <h3>{team_1_name}<br />{score}<br />{team_2_name}</h3>
+    <img src={team_2_logo_url} alt={item.name} style={w50pct} />
+    <img src={banner_url} alt={item.name} />
+  </div>
+  )
+}
+
--- src/layout/fetchContent.js
+++ src/layout/fetchContent.js
@@ -1,8 +1,7 @@
 /* Define front-end properties - Start */
 const token = 'a5e4f2d801eb4d338a7aad40a86544d5'
-const taxonomyCatName = 'Game Highlights'
-const matchHeroHeaderId = 'CORE0C26D9F3D73D4471BA72C0C8DE501087'
 const host = 'https://demo-oce0002.cec.ocp.oraclecloud.com/'
+const taxonomyCatName = 'Game Highlights'
 /* Define front-end properties - End */
 
 const itemsURL = ({ maxResults, sortOrder }) =>
@@ -16,6 +15,7 @@ const fetchItem = link => {
   return fetch(`${link.href}&expand=all`)
     .then(r => r.json())
 }
+
 const fetchItems = data => {
   const {ALL}= data
   const links = ALL.items
@@ -24,6 +24,7 @@ const fetchItems = data => {
   const itemFetches = links.map(fetchItem)
   return Promise.all(itemFetches).then(data => ({ALL, items:data}))
 }
+
 const fetches = ()=> fetch(
   itemsURL({ maxResults: 500, sortOrder: 'updatedDate:des' })
 )
